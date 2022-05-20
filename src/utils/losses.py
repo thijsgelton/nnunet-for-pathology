bb8b1f966874e334765e4809fccac520dc3cbee3
thijsgelton <thijs.gelton@hotmail.com>
2022-05-20 12:48:20 +0200

    Correction for ignoring first channel.

--- src/utils/losses.py
+++ src/utils/losses.py
@@ -3,14 +3,14 @@ from torch import nn
 
 
 class LossFactory(nn.Module):
-    def __init__(self, include_background=False, focal=False):
+    def __init__(self, ignore_first_channel=False, focal=False):
         super(LossFactory, self).__init__()
         if focal:
             self.loss_fn = DiceFocalLoss(
-                include_background=include_background, softmax=True, to_onehot_y=False, batch=True, gamma=2.0
+                include_background=not ignore_first_channel, softmax=True, to_onehot_y=False, batch=True, gamma=2.0
             )
         else:
-            self.loss_fn = DiceCELoss(include_background=include_background, softmax=True, to_onehot_y=False,
+            self.loss_fn = DiceCELoss(include_background=not ignore_first_channel, softmax=True, to_onehot_y=False,
                                       batch=True)
 
     def forward(self, y_pred, y_true):
